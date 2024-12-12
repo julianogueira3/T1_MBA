69358e914a2d7d4fe3158dd6d99f4e03f207322a
julianogueira3 <julia.mjn@icloud.com>
2024-12-12 19:50:07 -0300

    inicio do projeto

---
+++ lib/lib.py
@@ -0,0 +1,19 @@
+import os, csv
+from gdown import download
+
+# Definir a função download_from_drive
+def download_dataset(link,filename):
+
+    return None
+
+def preencher_matriz_contratos(nome_arquivo: str):
+    
+    return m, n, t, matriz     
+
+def imprimir_matriz(matriz, k=None):
+
+    return None
+
+def exportar_csv(nome_arquivo, matriz):
+
+    return None
